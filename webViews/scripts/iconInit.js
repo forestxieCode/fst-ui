e2259fbf6ef16f7f2d9f591ac9bd2b730771de7d
Forest_Xie <senlin_xie@egov-china.com>
2021-06-15 23:55:10 +0800

    feat:组件试图

---
+++ webViews/scripts/iconInit.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const postcss = require('postcss')
+const fs = require('fs')
+const path = require('path')
+const fontFile = fs.readFileSync(
+	require.resolve('element3/lib/theme-chalk/icon.css'),
+	'utf8'
+)
+
+const nodes = postcss.parse(fontFile).nodes
+const classList = []
+
+nodes.forEach((node) => {
+	const selector = node.selector || ''
+	const reg = new RegExp(/\.el-icon-([^:]+):before/)
+	const arr = selector.match(reg)
+
+	if (arr && arr[1]) {
+		classList.push(arr[1])
+	}
+})
+
+classList.reverse()
+
+fs.writeFile(
+	path.resolve(__dirname, '../src/icon.json'),
+	JSON.stringify(classList, null, 2) + '\n',
+	() => {}
+)
---
+++ webViews/src/main.js
@@ -0,0 +1,69 @@
+import Vue from 'vue'
+import EntryApp from './app'
+
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import routes from './route'
+import hljs from 'highlight.js'
+import demoBlock from './components/demo-block'
+import MainHeader from './components/header'
+import SideNav from './components/side-nav'
+import FooterNav from './components/footer-nav'
+import title from './i18n/title'
+
+import './demo-styles/index.scss'
+import './assets/styles/common.scss'
+import './assets/styles/fonts/style.css'
+import icon from './icon.json'
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+
+Vue.component('demo-block', demoBlock)
+Vue.component('main-header', MainHeader)
+Vue.component('side-nav', SideNav)
+Vue.component('footer-nav', FooterNav)
+
+const globalEle = new Vue({
+	data: { $isEle: false }, // 是否 ele 用户
+})
+
+Vue.mixin({
+	computed: {
+		$isEle: {
+			get: () => globalEle.$data.$isEle,
+			set: (data) => {
+				globalEle.$data.$isEle = data
+			},
+		},
+	},
+})
+
+Vue.prototype.$icon = icon // Icon 列表页用
+
+const router = new VueRouter({
+	mode: 'hash',
+	routes,
+})
+router.afterEach(async (route) => {
+	Vue.nextTick(() => {
+		const blocks = document.querySelectorAll('pre code:not(.hljs)')
+		Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+	})
+	const data = title
+	for (const val in data) {
+		if (new RegExp('^' + val, 'g').test(route.name)) {
+			document.title = data[val]
+			return
+		}
+	}
+	document.title = 'Element'
+})
+
+new Vue({
+	// eslint-disable-line
+	...EntryApp,
+	router,
+}).$mount('#app')
